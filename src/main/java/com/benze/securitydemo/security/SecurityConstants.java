f3b0d275647fe969c058ae2365450152077bb427
Sayan Das <94537198+BenzenEE@users.noreply.github.com>
2023-05-23 10:16:09 +0530

    Update SecurityConstants.java

--- src/main/java/com/benze/securitydemo/security/SecurityConstants.java
+++ src/main/java/com/benze/securitydemo/security/SecurityConstants.java
@@ -2,5 +2,5 @@ package com.benze.securitydemo.security;
 
 public class SecurityConstants {
 	public static final long JWT_EXPIRATION = 100000;
-    public static final String JWT_SECRET = "mention_your_secret_key";
+    public static final String JWT_SECRET = "mention_your_secret_key_here";
 }
